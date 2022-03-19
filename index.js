921ecaedfe1fad9d36edc4e0e1988db3c67cf228
Loyda <palacioloydita@gmail.com>
2022-03-19 00:19:33 -0700

    commit tree c878acad9a5b76a74ee28e3c482e0c01ed50b531

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+function returnFirstTwoDrivers() {
+    return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers() {
+    return drivers.slice(2, 4);
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number) {
+    return function () {
+        return number*number;
+    }
+}
+const fareDoubler = fare => fare*2
+const fareTripler = fare => fare*3
+
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(drivers);
+    
+}
